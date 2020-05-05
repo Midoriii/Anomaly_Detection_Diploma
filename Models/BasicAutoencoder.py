f0b5c01d97131166418ca78bb21cb6456533e400
Midoriii <stephen.Team24@gmail.com>
2020-05-05 17:39:22 +0200

    Refactoring BasicAutoencoder into it's own class, reconstructed imgs too

---
+++ Models/BasicAutoencoder.py
@@ -0,0 +1,71 @@
+import numpy as np
+
+from keras.layers import Input, Reshape, Dense, Flatten
+from keras.layers import Activation, Conv2D, MaxPooling2D, UpSampling2D, PReLU
+from keras.initializers import Constant
+from keras.models import Model
+from keras.callbacks import History
+
+'''
+Basic Convolutional Autoencoder
+
+@Author: Stepan Benes
+'''
+
+class BasicAutoencoder:
+
+    def __init__(self):
+        # Not entirely sure what encoded and decoded should be initialized as
+        self.encoded = Model()
+        self.decoded = Model()
+        self.model = Model()
+        self.history = History()
+        self.name = "BasicAutoencoder"
+        return
+
+
+    def create_net(self, input_shape):
+        net_input = Input(shape=input_shape)
+        x = Conv2D(32, (3, 3), padding='same')(net_input)
+        x = PReLU(alpha_initializer=Constant(value=0.25))(x)
+        x = MaxPooling2D((2, 2), padding='same')(x)
+
+        x = Conv2D(32, (3, 3), padding='same')(x)
+        x = PReLU(alpha_initializer=Constant(value=0.25))(x)
+        x = MaxPooling2D((2, 2), padding='same')(x)
+
+        x = Conv2D(32, (3, 3), padding='same')(x)
+        x = PReLU(alpha_initializer=Constant(value=0.25))(x)
+        self.encoded = MaxPooling2D((2, 2), padding='same')(x)
+
+        # And now the decoder part
+        x = Conv2D(32, (3, 3), padding='same')(self.encoded)
+        x = PReLU(alpha_initializer=Constant(value=0.25))(x)
+        x = UpSampling2D((2, 2))(x)
+
+        x = Conv2D(32, (3, 3), padding='same')(x)
+        x = PReLU(alpha_initializer=Constant(value=0.25))(x)
+        x = UpSampling2D((2, 2))(x)
+
+        x = Conv2D(32, (3, 3), padding='same')(x)
+        x = PReLU(alpha_initializer=Constant(value=0.25))(x)
+        x = UpSampling2D((2, 2))(x)
+        self.decoded = Conv2D(1, (3, 3), activation='sigmoid', padding='same')(x)
+
+        self.model = Model(net_input, self.decoded)
+        return
+
+
+    def compile_net(self):
+        self.model.compile(optimizer='adam', loss='mse')
+        self.model.summary()
+        return
+
+
+    def train_net(self, training_input, epochs, batch_size):
+        self.history = self.model.fit(training_input, training_input, epochs = epochs, batch_size=batch_size)
+        return
+
+
+    def predict(self, predict_input):
+        return self.model.predict(predict_input)
---
+++ autoencoder_tester.py
@@ -0,0 +1,59 @@
+import numpy as np
+import cv2
+from PIL import Image
+
+from Models.BasicAutoencoder import BasicAutoencoder
+
+import matplotlib.pyplot as plt
+
+
+
+# Notes : Sources say to not use dropout .. also that batch_norm may be an overkill, and also
+# to use simple ReLU ... dunno about padding='same' as well ..
+# Shouldn't the decoder be using transposeconv2D ?
+# They're so fcking blurry .. which might be useful
+
+
+img_width = 768
+img_height = 768
+input_shape = (img_width, img_height, 1)
+epochs = 30
+batch_size = 1
+
+
+# Load the previously stored data
+data = np.load("Data/OK.npy")
+#print(data.shape)
+
+# Reshape to fit the desired input
+data = data.reshape(data.shape[0], img_width, img_height, 1)
+
+# Normalize the data
+train_input = data.astype('float32') / 255.0
+#print(train_input.shape)
+
+# Let's create and compile a model
+basic_autoencoder = BasicAutoencoder()
+basic_autoencoder.create_net(input_shape)
+basic_autoencoder.compile_net()
+
+# Train it
+# Should add a plot of the loss in time
+basic_autoencoder.train_net(train_input, epochs=epochs, batch_size=batch_size)
+
+
+# To see the actual reconstructed images
+for i in range (0, train_input.shape[0]):
+    # Every image needs to be reshaped into 1,768,768,1
+    reconstructed_img = basic_autoencoder.predict(train_input[i].reshape(1, img_width, img_height, 1))
+    # The reconstructed image afterwards needs to be reshaped back into 768 x 768
+    reconstructed_img = reconstructed_img.reshape(img_width, img_height)
+
+    # Array has normalized values - need to multiply them again otherwise we get black picture
+    im = Image.fromarray(reconstructed_img * 255.0)
+    im = im.convert("L")
+    im.save('Reconstructed\\' + basic_autoencoder.name + str(i) + '.jpg')
+
+    #cv2.imshow("reconstructed", reconstructed_img)
+    #cv2.waitKey(0)
+    #cv2.imwrite('Reconstructed\\' + str(i) + '.jpg', reconstructed_img)
