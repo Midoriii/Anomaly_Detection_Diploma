18091ddacd7be001fcbb5e64410c8579c7e7c82b
Midoriii <stephen.Team24@gmail.com>
2020-07-23 17:14:32 +0200

    Initial Siamese Net model and tester

---
+++ Models/BaseSiameseModel.py
@@ -0,0 +1,58 @@
+import numpy as np
+
+from keras.optimizers import Adam
+from keras.models import Model
+from keras.callbacks import History
+
+'''
+Base Model for all Siamese Networks
+
+@Author: Stepan Benes
+'''
+
+class BaseSiameseModel:
+
+    def __init__(self):
+        # Embedding model is a single branch of the whole siamese network,
+        # used for predictions
+        self.embedding = Model()
+        self.model = Model()
+        self.history = History()
+        self.name = "BaseSiameseModel"
+        return
+
+
+    def create_net(self, input_shape):
+        raise NotImplementedError
+
+
+    def compile_net(self):
+        self.model.compile(optimizer=Adam(lr=0.0001), loss='binary_crossentropy', metrics=['accuracy'])
+        self.model.summary()
+        return
+
+
+    def train_net(self, training_input_left, training_input_right, training_labels, epochs, batch_size):
+        self.history = self.model.fit([training_input_left, training_input_right], training_labels, epochs = epochs, batch_size=batch_size)
+        return
+
+
+    def predict(self, predict_input):
+        return self.embedding.predict(predict_input)
+
+
+    def save_weights(self, epoch, batch_size):
+        self.model.save_weights('Model_Saves/Weights/' + self.name + '_e' + str(epoch) + '_b' + str(batch_size) + '_weights.h5')
+        return
+
+    def save_model(self, epoch, batch_size):
+        self.model.save('Model_Saves/Detailed/' + self.name + '_e' + str(epoch) + '_b' + str(batch_size) + '_detailed')
+        return
+
+    def save_embedding_weights(self, epoch, batch_size):
+        self.model.save_weights('Model_Saves/Weights/' + self.name + '_embedding_e' + str(epoch) + '_b' + str(batch_size) + '_weights.h5')
+        return
+
+    def save_embedding_model(self, epoch, batch_size):
+        self.model.save('Model_Saves/Detailed/' + self.name + '_embedding_e' + str(epoch) + '_b' + str(batch_size) + '_detailed')
+        return
---
+++ siamese_network_tester.py
@@ -0,0 +1,45 @@
+import numpy as np
+import cv2
+import sys, getopt
+import matplotlib.pyplot as plt
+
+from PIL import Image
+from Models.BasicSiameseNet import BasicSiameseNet
+
+
+img_width = 768
+img_height = 768
+input_shape = (img_width, img_height, 1)
+# Default params
+epochs = 20
+batch_size = 4
+
+# Loading data and labels
+left_data = np.load('Data/Test_Pairs_Left.npy')
+right_data = np.load('Data/Test_Pairs_Right.npy')
+labels = np.load('Data/Test_Pairs_Labels.npy')
+# Normalize the data
+left_data = left_data.astype('float32') / 255.0
+right_data = right_data.astype('float32') / 255.0
+
+
+model = BasicSiameseNet()
+# Create and compile the model
+model.create_net(input_shape)
+model.compile_net()
+#Train it
+model.train_net(left_data, right_data, labels, epochs=epochs, batch_size=batch_size)
+
+plt.plot(model.history.history['loss'])
+plt.title('model loss - ' + model.name)
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.savefig('Graphs/Losses/' + model.name + '_e' + str(epochs) + '_b' + str(batch_size) + '_loss.png', bbox_inches = "tight")
+plt.close('all')
+
+plt.plot(model.history.history['accuracy'])
+plt.title('model accuracy - ' + model.name)
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.savefig('Graphs/Accuracies/' + model.name + '_e' + str(epochs) + '_b' + str(batch_size) + '_acc.png', bbox_inches = "tight")
+plt.close('all')
