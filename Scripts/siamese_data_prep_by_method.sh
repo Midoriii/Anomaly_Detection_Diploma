86096a04574633c1978c3722432fdb64e28b6efa
Midoriii <stephen.Team24@gmail.com>
2020-07-24 15:13:38 +0200

    bugfix

--- Scripts/siamese_data_prep_by_method.sh
+++ Scripts/siamese_data_prep_by_method.sh
@@ -14,7 +14,7 @@ module add python36-modules-gcc
 module add opencv-3.4.5-py36
 
 
-cp -R $DATADIR/siamese_network_data_prep.py $DATADIR/Clonky-ok $DATADIR/Clonky-vadne $SCRATCHDIR
+cp -R $DATADIR/siamese_network_data_prep_by_method.py $DATADIR/Clonky-ok $DATADIR/Clonky-vadne $SCRATCHDIR
 
 cd $SCRATCHDIR
 
