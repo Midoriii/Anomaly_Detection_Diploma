235b1552fd8480e7b04a8d2f4bec8049c610db77
Midoriii <stephen.Team24@gmail.com>
2020-08-05 13:14:09 +0200

    stupid bugfix

--- siamese_predict_tester.py
+++ siamese_predict_tester.py
@@ -22,5 +22,3 @@ for i in range (0, pairs_labels.shape[0]):
                                pairs_right[i].reshape((1, img_width, img_height, 1)))
 
     print("Predicted: " + str(prediction) + ", Label: " + str(pairs_labels[i]))
-
-return
