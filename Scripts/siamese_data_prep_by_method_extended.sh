0fc928eceabc9f164b8b828f1b2ee0125a70da6f
Midoriii <stephen.Team24@gmail.com>
2020-08-13 22:40:49 +0200

    Deleted unnecessary scripts, made one to create extended pairs

---
+++ Scripts/siamese_data_prep_by_method_extended.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#PBS -l select=1:ncpus=1:mem=175gb:scratch_local=80gb
+#PBS -l walltime=3:00:00
+
+
+DATADIR=/storage/brno6/home/apprehension
+
+cd $DATADIR
+
+module add python-3.6.2-gcc
+module add python36-modules-gcc
+module add opencv-3.4.5-py36
+
+
+cp -R $DATADIR/siamese_network_data_prep_by_method.py $DATADIR/reshape_util.py $DATADIR/Clonky-ok $DATADIR/Clonky-vadne-full $SCRATCHDIR
+
+cd $SCRATCHDIR
+
+mkdir -p DataHuge
+mkdir -p Data
+
+
+python siamese_network_data_prep_by_method.py extended
+
+
+cp -vr $SCRATCHDIR/DataHuge/* $DATADIR/DataHuge/
+cp -vr $SCRATCHDIR/Data/* $DATADIR/Data/
+
+clean_scratch
