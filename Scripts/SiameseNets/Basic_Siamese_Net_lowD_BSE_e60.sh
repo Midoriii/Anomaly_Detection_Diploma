33db39087b7404b7c5a7822050a1cbef0e80e492
Midoriii <stephen.Team24@gmail.com>
2020-08-18 11:59:11 +0200

    walltime fix

--- Scripts/SiameseNets/Basic_Siamese_Net_lowD_BSE_e60.sh
+++ Scripts/SiameseNets/Basic_Siamese_Net_lowD_BSE_e60.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -q gpu
 #PBS -l select=1:ncpus=1:mem=175gb:ngpus=1:scratch_local=80gb
-#PBS -l walltime=15:00:00
+#PBS -l walltime=19:00:00
 
 
 DATADIR=/storage/brno6/home/apprehension
