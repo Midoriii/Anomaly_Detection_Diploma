3db144e82f15b9e3137a5d62e2669d809d5e380b
Midoriii <stephen.Team24@gmail.com>
2020-08-26 21:33:12 +0200

    Added docstrings to utils

--- granulo_utils.py
+++ granulo_utils.py
@@ -1,3 +1,25 @@
+'''
+A util module containing useful functions for performing granulometry related
+tasks on the given data.
+
+
+func: reshape_img_from_float(image, width, height): Denormalizes image from float32
+[0,1] to uint8 [0,255] and reshapes to dimensions (width, height).
+
+func: threshold_image(image, threshold): Performs binary thresholding on image
+with given threshold.
+
+func: remove_center(image): Removes 50x50 central area from image.
+
+func: plot_histogram(image): Calculates and shows the histogram of image.
+
+func: show_opening_contours(image, element, label): Shows thresholded image vs
+opened thresholded one with contours around the opening.
+
+func: granulometry_score(image, element): Performs granulometry on image,
+returns the sum of remaining white pixels after binary opening.
+'''
+
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -8,6 +30,20 @@ from scipy import ndimage
 
 
 def reshape_img_from_float(image, width, height):
+    '''
+    A helper method to denormalize and reshape previously saved image, turning it
+    from float32 in [0,1] to grayscale uint8 in [0,255] and reshaping it to
+    given dimensions for further processing.
+
+    Arguments:
+        image: A numpy array representing a 2D image, should be normalized.
+        width: Integer, desired width dimension.
+        height: Integer, desired height dimension.
+
+    Returns:
+        reshaped_image: A numpy array of uint8 values in range [0,255] shaped
+        into (width, height).
+    '''
     # Stretch out the values
     reshaped_image = image * 255.0
     # Reshape to desired size
@@ -21,6 +57,21 @@ def reshape_img_from_float(image, width, height):
 
 
 def threshold_image(image, threshold):
+    '''
+    Method that thresholds given image with binary thresholding, where values
+    below given threshold are set to 255 and values above are set to 0. This is
+    because granulometry works by counting remaining pixels, and in our data
+    the stains that we want to isolate and count are black or dark grey, hence
+    we need to set them to white (255), so granulometry can sum those pixels up,
+    after the binary opening is performed.
+
+    Arguments:
+        image: A numpy array representing a 2D image, in grayscale.
+        threshold: Integer value for binary thresholding.
+
+    Returns:
+        thresh: A numpy array containing the thresholded binary image.
+    '''
     # Threshold the image .. INV since we want to count white pixels that have
     # actual value in granulometry
     ret, thresh = cv2.threshold(np.array(image), threshold, 255, cv2.THRESH_BINARY_INV)
@@ -32,12 +83,31 @@ def threshold_image(image, threshold):
 
 
 def remove_center(image):
+    '''
+    Helper function to remove the center 50 by 50 part of images. This is because
+    this part contains the central hole that degraded granulometry performance.
+    The method expects images of size 768x768, which might be changed in the future.
+
+    Arguments:
+        image: A numpy array representing a 2D image with dimensions of 768x768.
+
+    Returns:
+        image: A numpy array identical to the given image with central 50x50 area
+        filled with 0s, effectively removing the area from granulometry, which
+        counts pixels with value > 0.
+    '''
     image[354:404,354:404] = 0
     return image
 
 
 def plot_histogram(image):
-    # Get Histogram
+    '''
+    Just a simple histogram visualisation of a given image. Works in grayscale.
+
+    Arguments:
+        image: A numpy array representing a 2D image.
+    '''
+    # Get Histogram of channel 0, since we're working with grayscale
     hist = cv2.calcHist([image], [0], None, [256], [0, 256])
     # Plot it
     plt.plot(hist)
@@ -46,7 +116,19 @@ def plot_histogram(image):
     plt.show()
 
 
-def show_opening_contours(image, opening_element, label=""):
+def show_opening(image, opening_element, label=""):
+    '''
+    A visualising method that shows the original thresholded image and the result
+    of binary opening with given element on the picture.
+
+    Arguments:
+        image: A preferably thresholded image on which the opening will be performed,
+        given as a numpy array.
+        opening_element: Array of 0s and 1s representing the element with which
+        to perform the opening.
+        label: Additional string, helps with distinguishing what kind of picture
+        is being worked with. I'm using it to distinguish between OK and faulty pics.
+    '''
     # Do the opening itself
     opening = ndimage.binary_opening(image, structure=opening_element)
     # Plot the opening
@@ -57,11 +139,26 @@ def show_opening_contours(image, opening_element, label=""):
     plt.subplot(122)
     plt.imshow(opening, cmap='gray')
     plt.title("Opened")
-    plt.contour(opening, [0.5], colors='b', linewidths=2)
+    # Plot the contours around the opening
+    #plt.contour(opening, [0.5], colors='b', linewidths=2)
     plt.show()
 
 
 def granulometry_score(image, opening_element):
+    '''
+    Preforms binary opening on given image and afterwards sums up the pixels.
+    Binary opening returns only 0s and 1s (False and True), so by simply
+    summing the pixels one can get a sort of 'score' of how much of the picture
+    remained after performing opening.
+
+    Arguments:
+        image: A numpy array representing a 2D image.
+        opening_element: Array of 0s and 1s representing an element with which
+        to perform the opening.
+
+    Returns:
+        granulo: An integer representing how many pixels remained after opening.
+    '''
     # Do the opening itself
     opening = ndimage.binary_opening(image, structure=opening_element)
     # Calc how many pixels of the image remain
--- reshape_util.py
+++ reshape_util.py
@@ -1,16 +1,37 @@
+'''
+A util module containing often used methods while saving / using data.
+
+
+func: crop_reshape(images): As name suggests, crops and reshapes a list of images.
+
+func: reshape_normalize(images, width, height): Normalizes input data and reshapes
+into (len(images), width, height, 1) format suitable for NN input.
+'''
+
 import numpy as np
 
 from cv2 import cv2
 
 
 def crop_reshape(images):
+    '''
+    Crops the additional info bar from images, reshapes them into desired
+    (768, 768) shape and converts to grayscale.
+
+    Arguments:
+        images: A list of images to perform operations on, should be 2D array.
+
+    Returns:
+        images_list: Numpy array of images converted to grayscale,
+        shaped into (768,768), and with cropped off info bar.
+    '''
     images_list = []
 
     for image in images:
         img = cv2.imread(image)
-        # Resize into 768*768 if bigger
+        # Resize into 768*840 if bigger
         resized = cv2.resize(img, (768, 840))
-        # Crop the bottom info
+        # Crop the bottom info, making it 768x768
         cropped_img = resized[0:768, 0:768]
         # Make it actual grayscale
         gray = cv2.cvtColor(cropped_img, cv2.COLOR_BGR2GRAY)
@@ -23,6 +44,18 @@ def crop_reshape(images):
 
 
 def reshape_normalize(images, img_width, img_height):
+    '''
+    Converts input image array into float32, normalizes it by dividing by 255.0,
+    and reshapes into a Keras Input type of shape; (len(images), width, height, 1).
+
+    Arguments:
+        images: Numpy array of images to peform operations on.
+        img_width: Integer, desired width dimension of images.
+        img_height: Integer, desired height dimension of images.
+
+    Returns:
+        reshaped: A numpy array of float32, normalized and reshaped images.
+    '''
     # Normalize the data
     normalized = images.astype('float32') / 255.0
     # Reshape into desirable shape
