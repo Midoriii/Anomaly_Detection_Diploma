0a9c6bd12e7437c1268880fe23a2cad9b429eb9f
Midoriii <stephen.Team24@gmail.com>
2020-08-28 14:27:29 +0200

    fixed contrastive loss

--- Models/Losses/custom_losses.py
+++ Models/Losses/custom_losses.py
@@ -11,8 +11,11 @@ def contrastive_loss(y_true, y_pred):
     Contrastive loss from Hadsell-et-al.'06
     http://yann.lecun.com/exdb/publis/pdf/hadsell-chopra-lecun-06.pdf
     '''
-    # Custom margin which should be attained between disimilar pairs
+    # Custom margin which should be attained between disimilar pairs.
+    # In our case, since we sigmoid the euclidean distance between feature
+    # spaces, the output is squashed between [0,1], so the maximum margin that
+    # makes sense is 1.
     margin = 1
     # Loss itself
-    return K.mean(y_true * K.square(y_pred)
-                  + (1 - y_true) * K.square(K.maximum(margin - y_pred, 0)))
+    return K.mean((1 - y_true) * K.square(y_pred)
+                  + y_true * K.square(K.maximum(margin - y_pred, 0)))
