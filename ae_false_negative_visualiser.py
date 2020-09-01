8e44a3bb52fce740767586b18db1715253f51a8d
Midoriii <stephen.Team24@gmail.com>
2020-09-01 17:46:25 +0200

    trailing whitespace removal

--- ae_false_negative_visualiser.py
+++ ae_false_negative_visualiser.py
@@ -10,7 +10,7 @@ detection.
 
 Upon showing an image that was falsely flagged as OK (and its reconstruction),
 any key press continues the script. The total amount of false negatives is also
-written to the command line after the script finishes. 
+written to the command line after the script finishes.
 '''
 import numpy as np
 
