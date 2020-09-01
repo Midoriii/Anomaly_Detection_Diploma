a65756eebb903fbdcd9e5089434dd11b83b7fc24
Midoriii <stephen.Team24@gmail.com>
2020-09-01 17:10:50 +0200

    Added docstring

--- granulometry_tester.py
+++ granulometry_tester.py
@@ -1,3 +1,37 @@
+'''
+The purpose of this script is to try granulometry on BSE type images, both OK
+and faulty ones.
+
+Granulometry could prove to be a simple and useful addition to Autoencoders,
+which struggle with defects that manifest as black dots on the image.
+Structuring element of good enough shape and size could filter out small, acceptable
+dots, while leaving the ones large enough to be marked as defects. Those are then
+summed up (the essence of granulometry) and if too many pixels remain, the image
+can be labeled as faulty.
+
+Normalized, float32 numpy arrays of images are loaded and afterwards stretched
+back into 768x768 and denormalized back to [0,255]. The grayscale images are then
+thresholded using either binary or gaussian adaptive thresholding, since we want
+only a binary image for image opening and granulometry. On BSE images, defects
+manifest as black shapes, so the goal of thresholding is to isolate those.
+
+The central part of thresholded image is then cut out, because it usually
+contains the central hole of the shutter, which messes up the granulometry. Not
+too many, if any, defects show in this small area, so it's rather safe to do so.
+
+Finally, binary image opening is performed with the defined structuring element.
+Pixels remaining are then counted and the sum functions as score of sorts. It's
+important to find out good enough threshold for this score, and it heavily depends
+on the thresholding performed and its parameters, as well as on the structuring element
+used and its size.
+
+Best performing params found so far are set as constants in this script. Unfortunately,
+it seems that the line between acceptable and faulty defects is too thin to be
+captured perfectly and even granulometry still leaves some false negatives. False
+positives are a lesser issue, but can stem from exceptionally low quality input image.
+
+@Author: Stepan Benes
+'''
 import numpy as np
 
 from cv2 import cv2
