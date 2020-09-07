2731f8d3fba508dab56e6b178bfa06b66983a787
Midoriii <stephen.Team24@gmail.com>
2020-09-07 21:39:16 +0200

    Yet another data prep script, this time for the extra ok images

---
+++ extra_ok_prep_by_method.py
@@ -0,0 +1,26 @@
+'''
+Short script to divide newly acquired OK data for testing purposes into
+BSE and SE data as per usual.
+'''
+import glob
+import numpy as np
+
+from reshape_util import crop_reshape
+from reshape_util import reshape_normalize
+
+
+
+IMG_WIDTH = 768
+IMG_HEIGHT = 768
+
+# Glob the images from the folder
+ok_images_se = glob.glob('Clonky-ok-2/*_3*')
+ok_images_bse = glob.glob('Clonky-ok-2/*_4*')
+
+# Remove the info bar from the images and reshape them into 768x768
+ok_images_se_list = crop_reshape(ok_images_se)
+ok_images_bse_list = crop_reshape(ok_images_bse)
+
+# Save the normalized data
+np.save("Data/SE_ok_extra.npy", reshape_normalize(ok_images_se_list, IMG_WIDTH, IMG_HEIGHT))
+np.save("Data/BSE_ok_extra.npy", reshape_normalize(ok_images_bse_list, IMG_WIDTH, IMG_HEIGHT))
