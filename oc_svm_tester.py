85f0ffafdb1cc9e79988b38a6049b9fa1f5b2f10
Midoriii <stephen.Team24@gmail.com>
2020-09-11 00:34:40 +0200

    Refactoring for better grid searching

--- oc_svm_tester.py
+++ oc_svm_tester.py
@@ -10,8 +10,8 @@ As OC-SVM requires extracted features as its training/testing input, a suitable
 pre-trained encoding (part of Autoencoders) or embedding (branch in Siamese Nets)
 model needs to be provided.
 
-After training is done, the OC-SVM is tested on extracted features of OK and Faulty
-data. The amount of False Negatives and False Positives is finally printed out.
+After training is done, the OC-SVM is tested on extracted features of extra OK and
+Faulty data. The amount of False Negatives and False Positives is finally printed out.
 
 Results will be added here.
 '''
@@ -28,7 +28,8 @@ IMG_HEIGHT = 768
 
 def main():
     '''
-    Loads OK and Faulty data and the encoding / embedding models and calls
+    Loads OK, Faulty and extra OK data and the encoding / embedding models, calls
+    extract_features() to get the features of the loaded data, and finally calls
     train_oc_svm(), once for each type of data, SE vs BSE.
     '''
     # Load SE and BSE ok and faulty data
@@ -44,55 +45,45 @@ def main():
     #model = load_model("Model_Saves/Detailed/encoder_extended_BasicAutoencoderEvenDeeperExtraLLR_e600_b4_detailed", compile=False)
     model = load_model("Model_Saves/Detailed/embedding_BasicSiameseNetLowerDropout_BSE_extended_e60_b4_detailed", compile=False)
     print("BSE:")
-    train_oc_svm(bse_ok_data, bse_faulty_data, bse_ok_data_extra, model)
+    # Extract features from each type of data
+    bse_ok_data_features = extract_features(bse_ok_data, model)
+    bse_faulty_data_features = extract_features(bse_faulty_data, model)
+    bse_ok_data_extra_features = extract_features(bse_ok_data_extra, model)
+    # Train the model and get the results
+    train_oc_svm(bse_ok_data_features, bse_faulty_data_features,
+                 bse_ok_data_extra_features, model)
+
     # Load SE embedding model
     model = load_model("Model_Saves/Detailed/embedding_BasicSiameseNetWithoutDropout_SE_extended_e40_b4_detailed", compile=False)
     print("SE:")
-    train_oc_svm(se_ok_data, se_faulty_data, se_ok_data_extra, model)
+    se_ok_data_features = extract_features(se_ok_data, model)
+    se_faulty_data_features = extract_features(se_faulty_data, model)
+    se_ok_data_extra_features = extract_features(se_ok_data_extra, model)
+    train_oc_svm(se_ok_data_features, se_faulty_data_features,
+                 se_ok_data_extra_features, model)
 
 
-def train_oc_svm(ok_data, faulty_data, testing_ok_data, model):
+def train_oc_svm(ok_data_features, faulty_data_features,
+                 testing_ok_data_features, model, nu_val=0.02):
     '''
-    Extracts the features of ok_data and faulty_data using the .predict() method
-    of given embedding or encoding model. One Class SVM is then trained on the
-    OK features. Predictions are then made on both Ok and Faulty data. Finally,
-    the number of false positives and false negatives is printed out.
+    Creates and trains an OC-SVM model on given OK features. Predictions are then
+    made on extra Ok and Faulty data. Finally, the number of false positives and
+    false negatives is printed out.
 
     Arguments:
-        ok_data: A numpy array of float32 [0,1] values representing images.
+        ok_data_features: A numpy array representing extracted features from OK data.
             Used for training.
-        faulty_data: A numpy array of float32 [0,1] values representing images.
+        faulty_data: A numpy array representing extracted features from Faulty data.
             Used for testing.
-        testing_ok_data: A numpy array of float32 [0,1] values representing images.
+        testing_ok_data: A numpy array representing extracted features from extra OK data.
             Used for testing.
-        model: An instantiated embedding or encoding model.
+        model: An instantiated pretrained embedding or encoding model.
+        nu_val: OC-SVM parameter, refer to:
+        https://scikit-learn.org/stable/modules/generated/sklearn.svm.OneClassSVM.html
     '''
-    ok_data_features = []
-    faulty_data_features = []
-    testing_ok_data_features = []
     # Create the OC-SVM model, gamma='auto' vs 'scale', 'auto' seems better
-    oc_svm_model = OneClassSVM(gamma='auto', nu=0.02)
-    # Get encodings of all OK, Faulty and testing OK images
-    for i in range(0, ok_data.shape[0]):
-        ok_data_features.append(model.predict(ok_data[i].reshape(1, IMG_WIDTH,
-                                                                 IMG_HEIGHT, 1)))
-    for i in range(0, faulty_data.shape[0]):
-        faulty_data_features.append(model.predict(faulty_data[i].reshape(1, IMG_WIDTH,
-                                                                         IMG_HEIGHT, 1)))
-    for i in range(0, testing_ok_data.shape[0]):
-        testing_ok_data_features.append(model.predict(testing_ok_data[i].reshape(1, IMG_WIDTH,
-                                                                                 IMG_HEIGHT, 1)))
-    # Convert to numpy arrays
-    ok_data_features = np.asarray(ok_data_features)
-    faulty_data_features = np.asarray(faulty_data_features)
-    testing_ok_data_features = np.asarray(testing_ok_data_features)
-    # Reshape to 2D for OC-SVM, -1 means 'make it fit'
-    ok_data_features = ok_data_features.reshape(ok_data_features.shape[0], -1)
-    faulty_data_features = faulty_data_features.reshape(faulty_data_features.shape[0], -1)
-    testing_ok_data_features = testing_ok_data_features.reshape(testing_ok_data_features.shape[0], -1)
-    #print(ok_data_features.shape)
-
-    # Train OC-SVM on OK images
+    oc_svm_model = OneClassSVM(gamma='auto', nu=nu_val)
+    # Train OC-SVM on OK image features
     oc_svm_model.fit(ok_data_features)
 
     # Predict on training OK data and Faulty data
@@ -105,6 +96,29 @@ def train_oc_svm(ok_data, faulty_data, testing_ok_data, model):
     print("FN:")
     print((faulty_predictions == 1).sum())
 
+
+def extract_features(images, model):
+    '''
+    Extracts features from given image data using model.predict() of a loaded
+    pretrained model.
+
+    Arguments:
+        images: A numpy array of float32 [0,1] values representing images.
+        model: An instantiated pretrained embedding or encoding model.
+
+    Returns:
+        A numpy array of extracted features for each input image.
+    '''
+    images_features = []
+
+    # Get encodings of all given images, using given model.predict()
+    for i in range(0, images.shape[0]):
+        images_features.append(model.predict(images_data[i].reshape(1, IMG_WIDTH,
+                                                                     IMG_HEIGHT, 1)))
+    # Convert to numpy array
+    images_features = np.asarray(images_features)
+    # Reshape to 2D for OC-SVM, -1 means 'make it fit'
+    return images_features.reshape(images_features.shape[0], -1)
 
 
 if __name__ == "__main__":
