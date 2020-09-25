0726c032c8b2ab9169b162bae85ba85dbbb16f23
Midoriii <stephen.Team24@gmail.com>
2020-09-25 15:13:45 +0200

    now shows FP/FN and undecided images

--- siamese_anomaly_predict_tester.py
+++ siamese_anomaly_predict_tester.py
@@ -3,20 +3,28 @@ A script for loading previously saved siamese network and performing
 anomaly prediction with it. Prediction is done using prototypes, based on
 the same principle as in siamese_network_tester.py.
 
-Script only outputs results to command line, it doesn't show the False Negatives
-or False Positives. Although such functionality could easily be added.
+Script outputs results to command line, it also shows the False Negatives
+and False Positives, along with undecided images.
 
 func: get_predictions(data, prototypes, model, faulty, image_type):
         Method that performs the prediction itself. Given data, prototypes and model,
         it compares each image with each of the 5 prototypes by using model.predict().
-        Afterwards for each image a verdict is printed in the command line.
+        Afterwards for each image a verdict is printed in the command line. FP, FN
+        and undecided images are also plotted.
 
 func: main(): Loads data, prototypes and model. Then calls get_predictions().
         Does so separately for each image type; BSE and SE.
 '''
 import numpy as np
 
+from cv2 import cv2
+
+from PIL import Image
 from keras.models import load_model
+
+# Constants
+IMG_WIDTH = 768
+IMG_HEIGHT = 768
 
 # Gets predictions for given data with given prototypes
 # Faulty and Img_Type denote the correctness of Data and type of Images
@@ -29,6 +37,7 @@ def get_predictions(data, data_prototypes, model, faulty="OK", img_type="BSE"):
     means an anomaly, anything in between requires further assistance, but these
     thresholds are arbitrary and can be customised. Verdict on each image is printed
     in the command line along with the actual state of the image (OK / Faulty).
+    False Positives/Negatives along with 'Don't know' images are also plotted.
 
     Arguments:
         data: Numpy array of float32 [0,1] images to be tested.
@@ -39,9 +48,6 @@ def get_predictions(data, data_prototypes, model, faulty="OK", img_type="BSE"):
         img_type: String that helps with distinction of the image type of data,
         either BSE or SE.
     '''
-    IMG_WIDTH = 768
-    IMG_HEIGHT = 768
-
     for i in range(0, data.shape[0]):
         score = 0
         verdict = ""
@@ -53,12 +59,39 @@ def get_predictions(data, data_prototypes, model, faulty="OK", img_type="BSE"):
 
         if score == 5:
             verdict = "OK"
+            if faulty == "Faulty":
+                show_missclassified_image(data[i], "False Negative")
         elif score == 0:
             verdict = "Faulty"
+            if faulty == "OK":
+                show_missclassified_image(data[i], "False Positive")
         else:
             verdict = "Don't know"
+            if faulty == "OK":
+                show_missclassified_image(data[i], "Don't know, OK image")
+            else:
+                show_missclassified_image(data[i], "Don't know, Faulty image")
 
         print(img_type + " " + faulty + " clonka #" + str(i) + ": " + verdict)
+
+
+def show_missclassified_image(img, msg):
+    '''
+    Reshapes, stretches and finally shows given image.
+
+    Arguments:
+        img: A numpy array of float32 [0,1] representing an image.
+        msg: Title to display when showing the image.
+    '''
+    # Image needs to be reshaped to 2D first
+    img = img.reshape(IMG_WIDTH, IMG_HEIGHT)
+    # Then it needs to be stretched back into [0,255] values
+    img = Image.fromarray(img * 255.0)
+    # Converted to grayscale
+    img = img.convert('L')
+    # And finally shown
+    cv2.imshow(msg, np.array(img))
+    cv2.waitKey(0)
 
 
 def main():
@@ -79,7 +112,7 @@ def main():
     # Loading best BSE Model
     # This one leaves 3 faulty as undecided
     model = load_model("Model_Saves/Detailed/BasicSiameseNetLowerDropout_BSE_extended_e60_b4_detailed", compile=False)
-    # This one leaves 3 OK as udnecided
+    # This one leaves 3 OK as undecided
     #model = load_model("Model_Saves/Detailed/SiameseNetLiteMultipleConvAltTwo_BSE_extended_e40_b4_detailed", compile=False)
 
     # First get the predictions for BSE OK and then Faulty images
