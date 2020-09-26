1966e1f19334520bb26b500f35c805cb9bce6782
Midoriii <stephen.Team24@gmail.com>
2020-09-26 11:39:57 +0200

    further outline

--- triplet_data_prep.py
+++ triplet_data_prep.py
@@ -12,11 +12,12 @@ def main():
     then saved.
     '''
     # Load BSE data - ok and extended faulty, leave ok extra for testing purposes
-    bse_ok = np.load("Data/BSE_ok.npy")
-    bse_faulty = np.load("Data/BSE_faulty_extended.npy")
+    # First try using low dim ones as they worked better for Siamese nets
+    bse_ok = np.load("Data/low_dim_BSE_ok.npy")
+    bse_faulty = np.load("Data/low_dim_BSE_faulty_extended.npy")
     # Load SE data too
-    se_ok = np.load("Data/SE_ok.npy")
-    se_faulty = np.load("Data/SE_faulty_extended.npy")
+    se_ok = np.load("Data/low_dim_SE_ok.npy")
+    se_faulty = np.load("Data/low_dim_SE_faulty_extended.npy")
     # Use only 80% OK and 75% Faulty data, leave rest for testing
     bse_ok = bse_ok[:int(0.8*len(bse_ok))]
     bse_faulty = bse_faulty[:int(0.75*len(bse_faulty))]
@@ -26,13 +27,13 @@ def main():
     bse_anchor, bse_pos, bse_neg = make_triplets(bse_ok, bse_faulty)
     se_anchor, se_pos, se_neg = make_triplets(se_ok, se_faulty)
     # Save them
-    np.save("DataTriplet/BSE_triplet_anchor.npy", bse_anchor)
-    np.save("DataTriplet/BSE_triplet_pos.npy", bse_pos)
-    np.save("DataTriplet/BSE_triplet_neg.npy", bse_neg)
+    np.save("DataTriplet/low_dim_BSE_triplet_anchor.npy", bse_anchor)
+    np.save("DataTriplet/low_dim_BSE_triplet_pos.npy", bse_pos)
+    np.save("DataTriplet/low_dim_BSE_triplet_neg.npy", bse_neg)
 
-    np.save("DataTriplet/SE_triplet_anchor.npy", se_anchor)
-    np.save("DataTriplet/SE_triplet_pos.npy", se_pos)
-    np.save("DataTriplet/SE_triplet_neg.npy", se_neg)
+    np.save("DataTriplet/low_dim_SE_triplet_anchor.npy", se_anchor)
+    np.save("DataTriplet/low_dim_SE_triplet_pos.npy", se_pos)
+    np.save("DataTriplet/low_dim_SE_triplet_neg.npy", se_neg)
 
 
 def make_triplets(ok_images, faulty_images):
@@ -46,6 +47,22 @@ def make_triplets(ok_images, faulty_images):
     Returns:
         triplets: [anchor, pos, neg] - Three lists of numpy arrays representing images.
     '''
+    anchor = []
+    pos = []
+    neg = []
+    # Append ok and faulty together to helper array
+
+    # While number of triplets < 5k
+    # Choose random index from 0 to len(helper_array)
+
+    # Pick Anchor by the random index and decide if it's OK or Faulty
+
+    # Get another Positive image and a Negative one
+
+    # Append each to their respective lists
+
+    # Concat the lists turned to numpy arrays and return them
+    return [np.array(anchor), np.array(pos), np.array(neg)]
 
 
 if __name__ == "__main__":
