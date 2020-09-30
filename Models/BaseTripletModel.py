f0165945f44d2d70139611fcef1c6d32b2433951
Midoriii <stephen.Team24@gmail.com>
2020-09-30 23:24:17 +0200

    added base triplet net

---
+++ Models/BaseTripletModel.py
@@ -0,0 +1,59 @@
+'''
+Base Model for all Triplet Networks
+'''
+import numpy as np
+
+from keras.optimizers import Adam
+from keras.models import Model
+from keras.callbacks import History
+
+from Models.Losses.custom_losses import triplet_loss
+
+
+
+class BaseTripletModel:
+
+    def __init__(self):
+        # Embedding model is a single branch of the whole triplet network,
+        # used for predictions
+        self.embedding = Model()
+        self.model = Model()
+        self.history = History()
+        self.name = "BaseTripletModel"
+        self.lr = 0.0001
+        return
+
+
+    def create_net(self, input_shape):
+        raise NotImplementedError
+
+
+    def compile_net(self):
+        self.model.compile(optimizer=Adam(lr=self.lr), loss=triplet_loss)
+        self.model.summary()
+        return
+
+    def train_net(self, anchor, positive, negative, epochs, batch_size):
+        # A dummy labels array
+        labels = np.empty((anchor.shape[0], 1))
+        self.history = self.model.fit([anchor, positive, negative], labels, epochs=epochs, batch_size=batch_size)
+        return
+
+    def predict(self, predict_input):
+        return self.embedding.predict(predict_input)
+
+    def save_weights(self, epoch, batch_size, type, extended_faulty, low_dims):
+        self.model.save_weights('Model_Saves/Weights/' + str(low_dims) + self.name + '_' + str(type) + str(extended_faulty) + '_e' + str(epoch) + '_b' + str(batch_size) + '_weights.h5')
+        return
+
+    def save_model(self, epoch, batch_size, type, extended_faulty, low_dims):
+        self.model.save('Model_Saves/Detailed/' + str(low_dims) + self.name + '_' + str(type) + str(extended_faulty) + '_e' + str(epoch) + '_b' + str(batch_size) + '_detailed')
+        return
+
+    def save_embedding_weights(self, epoch, batch_size, type, extended_faulty, low_dims):
+        self.embedding.save_weights('Model_Saves/Weights/embedding_' + str(low_dims) + self.name + '_' + str(type) + str(extended_faulty) + '_e' + str(epoch) + '_b' + str(batch_size) + '_weights.h5')
+        return
+
+    def save_embedding_model(self, epoch, batch_size, type, extended_faulty, low_dims):
+        self.embedding.save('Model_Saves/Detailed/embedding_' + str(low_dims) + self.name + '_' + str(type) + str(extended_faulty) + '_e' + str(epoch) + '_b' + str(batch_size) + '_detailed')
+        return
