326f4505bb9e34b0f1069e7309da3de4d49f0929
Midoriii <stephen.Team24@gmail.com>
2020-11-25 10:04:36 +0100

    reverted to 384x384 mode

--- bigan_data_prep.py
+++ bigan_data_prep.py
@@ -16,11 +16,11 @@ for file in ["low_dim_SE_ok", "low_dim_SE_ok_extra", "low_dim_SE_faulty_extended
     for i in range(data.shape[0]):
         img = data[i].reshape(384, 384)
         img = Image.fromarray(img, mode='L')
-        img = img.resize((192, 192))
+        img = img.resize((384, 384))
         img_list.append(np.asarray(img))
 
     imgs = np.array(img_list)
-    imgs = imgs.reshape(data.shape[0], 192, 192, 1)
+    imgs = imgs.reshape(data.shape[0], 384, 384, 1)
     imgs = imgs.astype('float32')
     print(data.shape)
     print(imgs.shape)
